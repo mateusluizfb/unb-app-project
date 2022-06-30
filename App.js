f0a5555d30ddc5555ef2e464b7622ace1bd0c505
Victor Henrique <victorhrvs@gmail.com>
2022-06-30 10:35:06 -0300

    Tela de Login

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './login';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Login></Login>
     </View>
+
   );
 }
 
